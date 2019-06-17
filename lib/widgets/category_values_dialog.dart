6f35828e6f9eb5f9b5a30853c23b3411b84da05d
mario <mario.schmidt4@gmx.net>
2019-06-18 00:50:56 +0200

    category_values_dialog: add textfields for value names

--- lib/widgets/category_values_dialog.dart
+++ lib/widgets/category_values_dialog.dart
@@ -6,20 +6,84 @@ class CategoryValuesDialog extends StatefulWidget {
 }
 
 class _CategoryValuesDialogState extends State<CategoryValuesDialog> {
+  var textControllerList = List<TextEditingController>();
+  var textControllerForNewItem = TextEditingController();
+  var values = List<String>();
+
+  void _removeValue(int index) {
+    setState(() {
+      values.removeAt(index);
+      textControllerList.removeAt(index);
+    });
+  }
+
+  void _addValue() {
+    setState(() {
+      textControllerList.add(TextEditingController(text: "${textControllerForNewItem.text}"));
+      values.add("${textControllerForNewItem.text}");
+      textControllerForNewItem.text = "";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: new Text("Category Values"),
-      content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            RaisedButton(
-              child: Text("OK"),
-              onPressed: () {
-                Navigator.of(context).pop(List<String>()..add("testB"));
-              },
-            )
-          ]
+      content: Container(
+        height: 900,
+        width: 600,
+        child: ListView.builder(
+          itemCount: values.length + 1,
+          itemBuilder: (context, index) {
+            bool last = values.length + 1 == (index + 1);
+
+            if (!last) {
+
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  onChanged: (text) => values[index] = text,
+                  controller: textControllerList[index],
+                  decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.remove),
+                        onPressed: () => _removeValue(index)
+                      )),
+                ),
+              );
+
+            } else {
+
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      controller: textControllerForNewItem,
+                      decoration: InputDecoration(
+                          hintText: "New Value",
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.add),
+                            onPressed: () => _addValue()
+                          )),
+                    ),
+
+                    RaisedButton(
+                      child: Text("OK"),
+                      onPressed: () {
+                        Navigator.of(context).pop(values);
+                      },
+                    ),
+
+                  ],
+                ),
+              );
+
+            }
+
+          }
+
+        ),
       ),
     );
   }
